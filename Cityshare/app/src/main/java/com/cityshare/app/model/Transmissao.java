cf3c842b50b2ac8840d90e8c09da9ded56a426aa
Leonardo Ponciano <leonardo@bellets.com>
2017-05-20 20:56:26 -0300

    comit

---
+++ Cityshare/app/src/main/java/com/cityshare/app/model/Transmissao.java
@@ -0,0 +1,26 @@
+package com.cityshare.app.model;
+
+/**
+ * Created by Leo on 5/20/2017.
+ */
+
+public class Transmissao {
+    private int id;
+    private String titulo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
